16b02dc0829a278c37163a5aac59f937fb34af3a
Kabirjeet Singh <kabir160804@gmail.com>
2025-07-21 22:19:08 +0530

    Updated Mobile Contact Me

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function updateDateTime() {
     const now = new Date();
- 
+
     const time = now.toLocaleTimeString('en-IN', {
         hour: '2-digit',
         minute: '2-digit',
         hour12: false
-    });    
+    });
 
     const timeElement = document.querySelector(".time");
     if (timeElement) {
@@ -15,3 +15,14 @@ function updateDateTime() {
 
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+function hideContactMeMobile() {
+    const contactDiv = document.getElementById("contactme_mobile");
+    if (contactDiv) {
+        contactDiv.style.display = "none";
+    }
+}
+
+function showContactMeMobile() {
+    document.getElementById("contactme_mobile").style.display = "flex";
+}
